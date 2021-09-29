537df552db9b3d27aae4ab18e6789fe8c08f2c44
Paola Cardona <paolajcarher@gmail.com>
2021-09-28 21:02:45 -0500

    Nueva funcionalidad: enviar registro--> funcion que verifica que todos los datos nombre, edad, contrasena son validos

--- validaciones.js
+++ validaciones.js
@@ -15,6 +15,24 @@ form_nuevo_usuario.addEventListener("submit", e=>{
 })*/
 
 
+function agregarRegistro(){
+    let dato_nombre_usuario= document.getElementById ("dato_nombre_usuario");
+    let dato_edad_usuario= document.getElementById ("dato_edad_usuario");
+    let dato_contrasena= document.getElementById ("dato_contrasena");
+
+    let nombreUsuarioValido=validar_nombre_usuario(dato_nombre_usuario.value);
+    let edadValida=validar_edad_usuario(dato_edad_usuario.value);
+    let contrasenaValida=validar_contrasena(dato_contrasena.value);
+
+    if (nombreUsuarioValido && edadValida && contrasenaValida){
+        alert("Datos validos");
+
+    
+    } else{
+        
+        alert("Datos NO validos");}
+}
+
 function validar_nombre_usuario(nombre){
         
     let ultimaletra=nombre[nombre.length-1];
@@ -24,6 +42,7 @@ function validar_nombre_usuario(nombre){
          //If an empty string ("") is used as the separator, the string is split between each character
     const myArr = nombre.split(" ");
     let validado= 0;
+    
 
     for (let i = 0; i < myArr.length; i++) {
             
@@ -31,10 +50,10 @@ function validar_nombre_usuario(nombre){
             let tieneEspaciosInicioPalabra=myArr[i][0].match(/\s/);
 
             if (esMayusculaPrimeraLetraPalabra_n!=null && tieneEspaciosInicioPalabra==null && tieneEspaciosFinal==null ){
-                alert("primera letra de la primera palabra es mayuscula no termina en espacio y no tiene espacios inicio"+  myArr[i]); 
+               // alert("primera letra de la primera palabra es mayuscula no termina en espacio y no tiene espacios inicio"+  myArr[i]); 
                 validado=validado+1;               
                 }else{
-                    alert("primera letra de la primera palabra es empieza minuscula+"+myArr[i]);
+                  //  alert("primera letra de la primera palabra es empieza minuscula+"+myArr[i]);
                     validado=0;
                 }
         }
@@ -58,7 +77,9 @@ function validar_contrasena(contrasena){
     var valida= contrasena.match(/^[A-Za-z0-9]+$/) 
     if (valida!=null & contrasena.length>=6) {
         alert("contrasena valida")
+        
         return true;
+        
     }else{
         alert("contrasena no valida")
         return false;
@@ -75,23 +96,24 @@ function validar_edad_usuario(edad){
         if (edad > 0) { //edad es positiva
             if ((edad >= 13) && (edad < 110)) {
                 alert("EDAD valida");
+               
                 return true; //edad esta en el rango
             } else {
                 alert("EDAD NO valida");
                 return false;// edad no esta en el rango
             }
         } else {
-            alert("EDAD NO valida");
+        //    alert("EDAD NO valida");
             return false;// edad  is negative
             
         }
     }
 }
-
-
+/*
+module.exports.agregarRegistro=agregarRegistro;
 module.exports.validar_nombre_usuario=validar_nombre_usuario;
 module.exports.validar_contrasena=validar_contrasena;
-module.exports.validar_edad_usuario=validar_edad_usuario;
+module.exports.validar_edad_usuario=validar_edad_usuario;*/
 
 
 /*
